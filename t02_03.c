0ccd6f94f4157b69db6773826d3ae25349c26982
si25itdel <si25itdel@gmail.com>
2026-04-07 21:33:58 +0700

    12S25006
    DONE

--- t02_03.c
+++ t02_03.c
@@ -1,35 +1,39 @@
 // 12S25006 - Rafael Dietrich Z Simatupang
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc)
 {
     char op;
-    scanf(" %c", &op); 
+    scanf(" %c", &op);
 
     int hasil = 0;
     if (op == '*') {
         hasil = 1;
     }
 
-    int num;
-    for (int i = 0; i < 4; i++) {
-        scanf("%d", &num);
-        
-        if (num == -1) {
+    int angka;
+    int iterasi = 0;
+
+    while (iterasi < 4) {
+        scanf("%d", &angka);
+
+        if (angka == -1) {
             printf("0\n");
             break;
         }
 
         if (op == '+') {
-            hasil += num;
+            hasil = hasil + angka;
         } else if (op == '-') {
-            hasil -= num;
+            hasil = abs(hasil - angka);
         } else if (op == '*') {
-            hasil *= num;
+            hasil = hasil * angka;
         }
-        
+
         printf("%d\n", hasil);
+        iterasi++;
     }
 
     return 0;
